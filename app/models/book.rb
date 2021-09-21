ebe2e2734071f634435bcd04781acb3c222a3b57
mikecm <mikecm@gmail.com>
2021-09-20 18:43:49 -0600

    Create has_many relationships with reviews

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :author
 
+  has_many :reviews, dependent: :destroy
+
   validates :title, :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
   validates :first_name, :last_name, presence: true
 end
